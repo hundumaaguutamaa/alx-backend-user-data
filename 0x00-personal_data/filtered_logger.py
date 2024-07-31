35e3edc5373c7b1703fffe015745ad76581719ce
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-07-31 13:50:42 +0000

    function called filter_datum that returns the log message obfuscated:

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env python3
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+        fields (list): A list of strings representing all fields to obfuscate.
+        redaction (str): A string representing by what the field will be obfuscated.
+        message (str): A string representing the log line.
+        separator (str): A string representing the character that separates all fields in the log line.
+
+    Returns:
+        str: The log message with specified fields obfuscated.
+    """
+    pattern = '|'.join(f'{field}=[^{separator}]+' for field in fields)
+    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
+
