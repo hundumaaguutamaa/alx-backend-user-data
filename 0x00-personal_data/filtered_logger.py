1993bbd0496fc489c232a1313aa2357d8592c86c
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-01 04:04:54 +0300

    New line added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,3 +5,4 @@ def filter_datum(fields, redaction, message, separator):
     return re.sub(r'({})=[^{}]*'.format('|'.join(re.escape(field) for field in fields), re.escape(separator)), 
                   lambda m: m.group(0).split('=')[0] + '=' + redaction, 
                   message)
+    
