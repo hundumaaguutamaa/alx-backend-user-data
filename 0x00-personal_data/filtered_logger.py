e1477ee6586fd1d7a4ed6e2cc40a36f1345a9590
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-01 03:58:13 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,7 @@
+#!/usr/bin/env python3
 import re
 
 def filter_datum(fields, redaction, message, separator):
-    """
-    Obfuscates specified fields in a log message.
-
-    Args:
-        fields (list): A list of strings representing all fields to obfuscate.
-        redaction (str): A string representing by what the field will be obfuscated.
-        message (str): A string representing the log line.
-        separator (str): A string representing the character that separates all fields in the log line.
-
-    Returns:
-        str: The log message with specified fields obfuscated.
-    """
-    pattern = '|'.join(f'{field}=[^{separator}]+' for field in fields)
-    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
-
+    return re.sub(r'({})=[^{}]*'.format('|'.join(re.escape(field) for field in fields), re.escape(separator)), 
+                  lambda m: m.group(0).split('=')[0] + '=' + redaction, 
+                  message)
