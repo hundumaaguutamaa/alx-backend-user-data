ee0ef4b4b66c4ef2478af96eac036d10249b12cb
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-01 04:42:16 +0300

    check valid password added

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -30,4 +30,3 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     # Check if the password matches the hashed password
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
