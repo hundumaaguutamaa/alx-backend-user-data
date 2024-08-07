3ac6fcef5b6435d9a1d24dd0ea3577c7c4a0ad02
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-07 04:57:46 +0300

    Added methos for Base64 auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,5 +10,12 @@ class BasicAuth(Auth):
     BasicAuth class inherits from Auth.
     This class will use basic authentication, but is empty for now.
     """
-    pass
-
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
+        
