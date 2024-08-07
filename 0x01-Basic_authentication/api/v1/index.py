06c5ebc74dea597305a8b0eda1e67bde29619a4c
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-07 01:07:37 +0000

    index.py

---
+++ 0x01-Basic_authentication/api/v1/index.py
@@ -0,0 +1,8 @@
+from flask import abort, jsonify
+from api.v1.views import app_views
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_route():
+    """ Route to trigger a 401 error for testing """
+    abort(401)
+
