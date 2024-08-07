b4887195ebf3308f92356d9306182614f06733ed
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-08-07 04:43:50 +0300

    Docstring added

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""Authentication module.
+"""
+Authentication module.
 """
 
 from flask import request
 from typing import List, TypeVar
 
 class Auth:
+    """
+    Auth class to manage the API authentication.
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if a given path requires authentication.
@@ -19,14 +24,14 @@ class Auth:
         """
         if path is None:
             return True
-        
+
         if excluded_paths is None or not excluded_paths:
             return True
 
         # Ensure the path ends with a slash for comparison
         if not path.endswith('/'):
             path += '/'
-        
+
         for excluded_path in excluded_paths:
             if excluded_path.endswith('/') and path == excluded_path:
                 return False
@@ -45,12 +50,8 @@ class Auth:
         """
         if request is None:
             return None
-        
-        auth_header = request.headers.get('Authorization')
-        if auth_header is None:
-            return None
 
-        return auth_header
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
