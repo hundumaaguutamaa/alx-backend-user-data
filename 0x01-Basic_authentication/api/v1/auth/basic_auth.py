2fb3c7511c2f84220ecd9cba1cfd82753b09d8a3
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-08-07 01:51:01 +0000

    basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication module.
+"""
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class inherits from Auth.
+    This class will use basic authentication, but is empty for now.
+    """
+    pass
+
